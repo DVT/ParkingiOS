b94c05872007b833865d617b2d2c4bac53a31180
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-03-10 11:43:38 +0200

    Creation of Beginner Colour Palette
    The two colours Purplish and PurpleBlue have been added to the Assets File. To access them you can either go to the storyboard and they will be under named colours or access them in code through the ColourTheme.Palette string.

---
+++ ParkIt/ParkIt/Colour/ColourTheme.swift
@@ -0,0 +1,18 @@
+//
+//  ColourTheme.swift
+//  ParkIt
+//
+//  Created by Akua Afrane-Okese on 2020/03/10.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Universal Colours to use
+public class ColourTheme {
+    public struct Palette {
+        static let primaryPurple = UIColor(named: "Purplish")
+        static let secondaryPurple = UIColor(named: "PurpleBlue")
+    }
+}
