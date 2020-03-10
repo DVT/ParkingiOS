6d521bd00e374498edf14112dc29a4b29dbee435
NathanDVT <nngobale@jhb.dvt.co.za>
2020-03-10 16:37:10 +0200

    teamiOS

---
+++ ParkIt/ParkIt/CoreClasses/BankCard.swift
@@ -0,0 +1,22 @@
+//
+//  BankCard.swift
+//  ParkIt
+//
+//  Created by Tiewhan Smith on 2020/03/10.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+import Foundation
+
+class BankCard {
+  
+  var cardNumber: String
+  var csvNumber: String
+  var expiryDate: String
+  
+  init(cardNumber: String, csvNumber: String, expiryDate: String) {
+    self.cardNumber = cardNumber
+    self.csvNumber = csvNumber
+    self.expiryDate = expiryDate
+  }
+  
+}
