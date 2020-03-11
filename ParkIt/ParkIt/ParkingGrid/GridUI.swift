1f6b7a5529d4f4209cd79a21ff250e3928750646
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-03-11 14:44:24 +0200

    inserted the code to display the data onto the grid when the data is received via the "viewDidAppear" function and created a timer that continuously calls a function that gets the data and assigns the array every 30 seconds

--- ParkIt/ParkIt/ParkingGrid/GridUI.swift
+++ ParkIt/ParkIt/ParkingGrid/GridUI.swift
@@ -12,45 +12,51 @@ class GridUI: UIViewController {
   @IBOutlet weak var LeftStack: UIStackView!
   @IBOutlet weak var RightStack: UIStackView!
   var parkingSpaces = [ParkingSpot]()
+  var gameTimer: Timer?
   
     override func viewDidLoad() {
         super.viewDidLoad()
 //      this will change when the array reccieved
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .occupied))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .vacant))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
-      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
- 
-      var counter = 0
-      for spot in parkingSpaces {
-        if (counter<5) {
-          let iconName = selecticontodisplay(spot: spot)
-          let image = UIImageView(image: UIImage(named: iconName))
-          if (spot.type == .disabled) {
-            image.backgroundColor = UIColor.brown
-          }
-          image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-           LeftStack.addArrangedSubview(image)
-        } else if (counter>4 && counter<parkingSpaces.count) {
-           let iconName = selecticontodisplay(spot: spot)
-          let image = UIImageView(image: UIImage(named: iconName))
-          if (spot.type == .disabled) {
-//            this is for disables parking spaces.
-            image.backgroundColor = UIColor.brown
-          }
-          image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-          RightStack.addArrangedSubview(image)
-        }
-        counter = counter + 1
-      }
       
     }
+  
+  override func viewDidAppear(_ animated: Bool) {
+//    this line runs the runTimedCode function continuously
+    gameTimer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+  }
+  @objc func runTimedCode() {
+    let myFire = FirebaseRetrieveData()
+      myFire.getData(parkingLevel: "Level0") { (parking) in
+      self.parkingSpaces = parking
+      DispatchQueue.main.async {
+       var counter = 0
+        for spot in self.parkingSpaces {
+          if (counter<(self.parkingSpaces.count/2)) {
+                  let iconName = self.selecticontodisplay(spot: spot)
+                  let image = UIImageView(image: UIImage(named: iconName))
+                  if (spot.type == .disabled) {
+                    image.backgroundColor = UIColor.brown
+                  }
+                  image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+                  self.LeftStack.addArrangedSubview(image)
+          } else if (counter>((self.parkingSpaces.count/2)-1) && counter<self.parkingSpaces.count) {
+                  let iconName = self.selecticontodisplay(spot: spot)
+                  let image = UIImageView(image: UIImage(named: iconName))
+                  if (spot.type == .disabled) {
+        //            this is for disables parking spaces.
+                    image.backgroundColor = UIColor.brown
+                  }
+                  image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+                  self.RightStack.addArrangedSubview(image)
+                }
+                counter = counter + 1
+              }
+      }
+    }
+  }
+  
+//  WE WILL NEED TO DEINITIALISE THE TIMER WHEN WE MOVE TO ANOTHER AREA WITH: gameTimer?.invalidate()
+//
   public func selecticontodisplay(spot: ParkingSpot) -> String {
     var icon: String = ""
     if (spot.status == .occupied) {
