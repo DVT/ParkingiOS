e3a344d3247d7cdc071045da2f1ebe6e89b6ed10
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-03-11 08:59:08 +0200

    Add function to create the circular progress view

--- ParkIt/ParkIt/CircularProgressView.swift
+++ ParkIt/ParkIt/CircularProgressView.swift
@@ -13,19 +13,51 @@ class CircularProgressView: UIView {
   private var progressLayer = CAShapeLayer()
   private var trackLayer = CAShapeLayer()
   
-  var ProgressColor: UIColor = UIColor.white {
+  var progressColor: UIColor = UIColor.white {
     
     didSet {
-      progressLayer.strokeColor = ProgressColor.cgColor
+      progressLayer.strokeColor = progressColor.cgColor
     }
     
   }
   
-  var TrackColor: UIColor = UIColor.black {
+  var trackColor: UIColor = UIColor.black {
     
     didSet {
-      trackLayer.strokeColor = TrackColor.cgColor
+      trackLayer.strokeColor = trackColor.cgColor
     }
+    
+  }
+  
+  private func createCircularPath() {
+    
+    self.backgroundColor = UIColor.clear
+    self.layer.cornerRadius = self.frame.size.width/2
+    
+    let arcCenter = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+    let radius = (frame.size.width - 1.5)/2
+    let startAngle = CGFloat(-0.5 * .pi)
+    let endAngle = CGFloat(1.5 * .pi)
+    
+    let circlePath = UIBezierPath(arcCenter: arcCenter,
+                                  radius: radius,
+                                  startAngle: startAngle,
+                                  endAngle: endAngle,
+                                  clockwise: true)
+    
+    trackLayer.path = circlePath.cgPath
+    trackLayer.fillColor = UIColor.clear.cgColor
+    trackLayer.strokeColor = trackColor.cgColor
+    trackLayer.lineWidth = 10.0
+    trackLayer.strokeEnd = 1.0
+    layer.addSublayer(trackLayer)
+    
+    progressLayer.path = circlePath.cgPath
+    progressLayer.fillColor = UIColor.clear.cgColor
+    progressLayer.strokeColor = progressColor.cgColor
+    progressLayer.lineWidth = 10.0
+    progressLayer.strokeEnd = 0.0
+    layer.addSublayer(progressLayer)
     
   }
   
