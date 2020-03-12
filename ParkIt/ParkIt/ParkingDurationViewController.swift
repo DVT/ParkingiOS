d54d540288107b41a20bd94221c31f7547c0f3b9
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-03-12 10:00:35 +0200

    Add text and visual timing functions

--- ParkIt/ParkIt/ParkingDurationViewController.swift
+++ ParkIt/ParkIt/ParkingDurationViewController.swift
@@ -10,41 +10,64 @@ import UIKit
 
 class ParkingDurationViewController: UIViewController {
 
+  @IBOutlet weak var lblPlace: UILabel!
+  @IBOutlet weak var lblCost: UILabel!
+  @IBOutlet weak var lblTime: UILabel!
+  
   private var circularView: CircularProgressView?
   
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      setUpCircularProgressView()
+      if let view = self.view.viewWithTag(10) {
+        setUpCircularProgressView(in: view, lasting: 90)
+      }
       
     }
   
-  func setUpCircularProgressView() {
+  private func incrementTime(runCount: Int) {
     
-    guard var circularView = circularView else {
+    let minutes = runCount % 60
+    let hours = Int(floor(Double(runCount)/60.0))
+    
+    let labelContent = "\(hours):\(minutes)"
+    lblTime.text = labelContent
+    
+  }
+  
+  private func setUpCircularProgressView(in parentView: UIView, lasting duration:Int) {
+    
+    circularView = CircularProgressView(frame: CGRect(x: 0.0, y: 0.0, width: 230.0, height: 230.0))
+    
+    guard let circularView = circularView else {
       return
     }
     
-    circularView = CircularProgressView(frame: CGRect(x: 10.0, y: 10.0, width: 250.0, height: 250.0))
     circularView.trackColor = ColourTheme.Palette.primaryPurple ?? .red
     circularView.progressColor = ColourTheme.Palette.secondaryPurple ?? .blue
     
-    self.view.addSubview(circularView)
+    parentView.backgroundColor = .white
+    parentView.addSubview(circularView)
     
-    circularView.center = self.view.center
+    circularView.center = parentView.center
     circularView.resetWhenDone = true
     
     var runCount = 0
     
     let _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
       
+      if runCount >= 120 {
+        timer.invalidate()
+        return
+      }
+      
       self.circularView?.addToProgress(add: (1/60), duration: 1.0)
       
       runCount += 1
       
-      if runCount >= 120 {
-        timer.invalidate()
-      }
+      self.incrementTime(runCount: runCount)
+      
+      
       
     }
     
