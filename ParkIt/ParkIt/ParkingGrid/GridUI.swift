2fa16fb7875055ec020aac71d18db0458460e55d
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-03-12 10:09:59 +0200

    removed crashing code and attempted to make code more efficient

--- ParkIt/ParkIt/ParkingGrid/GridUI.swift
+++ ParkIt/ParkIt/ParkingGrid/GridUI.swift
@@ -8,57 +8,72 @@
 
 import UIKit
 
+extension UIStackView {
+    
+    func removeAllArrangedSubviews() {
+        
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+        
+        // Deactivate all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+        
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
+    }
+}
+
 class GridUI: UIViewController {
   @IBOutlet weak var LeftStack: UIStackView!
   @IBOutlet weak var RightStack: UIStackView!
   var parkingSpaces = [ParkingSpot]()
-  var gameTimer: Timer?
+  var parkTimer: Timer?
   
     override func viewDidLoad() {
         super.viewDidLoad()
-//      this will change when the array reccieved
-      
     }
-  
   override func viewDidAppear(_ animated: Bool) {
-//    this line runs the runTimedCode function continuously
+    //    this line runs the runTimedCode function continuously
     runTimedCode()
-    gameTimer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+    parkTimer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
   }
+  
   @objc func runTimedCode() {
     let myFire = FirebaseRetrieveData()
       myFire.getData(parkingLevel: "Level0") { (parking) in
       self.parkingSpaces = parking
-      DispatchQueue.main.async {
-       var counter = 0
+        DispatchQueue.main.async {
+           var counter = 0
         for spot in self.parkingSpaces {
 //          bug for view, empty the stackviews everytime the function is called
-          if (counter<(self.parkingSpaces.count/2)) {
+          if counter < 2 {
                   let iconName = self.selecticontodisplay(spot: spot)
                   let image = UIImageView(image: UIImage(named: iconName))
                   if (spot.type == .disabled) {
                     image.backgroundColor = UIColor.brown
                   }
                   image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-                  self.LeftStack.addArrangedSubview(image)
-          } else if (counter>((self.parkingSpaces.count/2)-1) && counter<self.parkingSpaces.count) {
-                  let iconName = self.selecticontodisplay(spot: spot)
-                  let image = UIImageView(image: UIImage(named: iconName))
-                  if (spot.type == .disabled) {
-        //            this is for disables parking spaces.
-                    image.backgroundColor = UIColor.brown
-                  }
-                  image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-                  self.RightStack.addArrangedSubview(image)
-                }
-                counter = counter + 1
+            self.LeftStack.insertArrangedSubview(image, at: counter)
+          } else if (counter > 1) && (counter < self.parkingSpaces.count) {
+              let iconName = self.selecticontodisplay(spot: spot)
+              let image = UIImageView(image: UIImage(named: iconName))
+              if (spot.type == .disabled) {
+    //            this is for disables parking spaces.
+                image.backgroundColor = UIColor.brown
               }
+              image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+              self.RightStack.insertArrangedSubview(image, at: (counter-2))
+            }
+          counter = counter + 1
+          print(counter)
+        }
       }
     }
   }
-  
 //  WE WILL NEED TO DEINITIALISE THE TIMER WHEN WE MOVE TO ANOTHER AREA WITH: gameTimer?.invalidate()
-//
+
   public func selecticontodisplay(spot: ParkingSpot) -> String {
     var icon: String = ""
     if (spot.status == .occupied) {
