ced34806b2ef39814de80cc07ba122a7454f601e
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-03-12 15:10:54 +0200

    Fix code structure causing breaks in view controller

--- ParkIt/ParkIt/ViewController.swift
+++ ParkIt/ParkIt/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Brandon Gouws on 2020/03/09.
 //  Copyright © 2020 DVT. All rights reserved.
 //
-
 import UIKit
 import Firebase
 
@@ -25,23 +24,12 @@ class ViewController: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "WelcomeViewController") as UIViewController
         present(vc, animated: true, completion: nil)
     }
-  @IBAction func btnParkingDuration(_ sender: Any) {
-    
-    let storyboard = UIStoryboard(name: "ParkingDuration", bundle: nil)
-    let vc = storyboard.instantiateViewController(withIdentifier: "ParkingDurationViewController") as? ParkingDurationViewController
-    
-    if let viewController = vc {
-      
-      viewController.parkingLevel = ParkingFloor(level: .ground, info: "Info", rate: 17.00, parkingDetails: "Details", parkingSpots: [], stores: "Spar", "Pick 'n Pay")
-      present(viewController, animated: true, completion: nil)
     @IBAction func dashboardButtonTapped(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "DashboardStoryboard", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "DashboardVC") as UIViewController
         present(vc, animated: true, completion: nil)
     }
     
-  }
-  
     //Logging in dummy user
     @IBOutlet weak var lblVacant: UILabel!
     override func viewDidLoad() {
@@ -55,4 +43,18 @@ class ViewController: UIViewController {
             }
         }
     }
+  
+  @IBAction func btnParkingDuration(_ sender: Any) {
+    
+    let storyboard = UIStoryboard(name: "ParkingDuration", bundle: nil)
+    let vc = storyboard.instantiateViewController(withIdentifier: "ParkingDurationViewController") as? ParkingDurationViewController
+    
+    if let viewController = vc {
+      
+      viewController.parkingLevel = ParkingFloor(level: .ground, info: "Info", rate: 17.00, parkingDetails: "Details", parkingSpots: [], stores: "Spar", "Pick 'n Pay")
+      present(viewController, animated: true, completion: nil)
+    
+    }
+  }
+  
 }
