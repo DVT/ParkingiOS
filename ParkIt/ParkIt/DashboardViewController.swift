8e2cb16352ec449bca46114865cbe59c82e3246f
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-03-12 07:26:21 +0200

    Added Search bar to Dashboard
    The dashboard can now search for Rosebank Mall.

--- ParkIt/ParkIt/DashboardViewController.swift
+++ ParkIt/ParkIt/DashboardViewController.swift
@@ -7,13 +7,59 @@
 //
 
 import UIKit
+import DropDown
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    var data: [String] = ["Rosebank Mall"]
+    var dataFiltered: [String] = []
+    var dropButton = DropDown()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+       dataFiltered = data
 
-        // Do any additional setup after loading the view.
+        dropButton.anchorView = searchBar
+        dropButton.bottomOffset = CGPoint(x: 0, y:(dropButton.anchorView?.plainView.bounds.height)!)
+        dropButton.backgroundColor = .white
+        dropButton.direction = .bottom
+
+        dropButton.selectionAction = { [unowned self] (index: Int, item: String) in
+            print("Selected item: \(item) at index: \(index)") //Selected item: code at index: 0
+            self.searchBar.text = item
+        }
+        searchBar.delegate = self
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        dataFiltered = searchText.isEmpty ? data : data.filter({ (dat) -> Bool in
+            dat.range(of: searchText, options: .caseInsensitive) != nil
+        })
+
+        dropButton.dataSource = dataFiltered
+        dropButton.show()
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+        for ob: UIView in ((searchBar.subviews[0] )).subviews {
+            if let z = ob as? UIButton {
+                let btn: UIButton = z
+                btn.setTitleColor(UIColor.white, for: .normal)
+            }
+        }
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.text = ""
+        dataFiltered = data
+        dropButton.hide()
     }
     
 
