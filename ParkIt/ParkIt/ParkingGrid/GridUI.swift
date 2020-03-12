346687b4e688693ccef45220e7d262f4989d002e
NathanDVT <nngobale@jhb.dvt.co.za>
2020-03-12 16:17:33 +0200

    Added selection functionality to the Grid screen

--- ParkIt/ParkIt/ParkingGrid/GridUI.swift
+++ ParkIt/ParkIt/ParkingGrid/GridUI.swift
@@ -16,14 +16,15 @@ extension ParkingSpot {
             case .levelOne: return 1
             }
         }
+    }
 }
-}
-
 
 class GridUI: UIViewController {
+    var selectedIndex = -1
   @IBOutlet weak var LeftStack: UIStackView!
   @IBOutlet weak var RightStack: UIStackView!
-  var parkingSpaces = [ParkingSpot]()
+    @IBOutlet weak var lblSelection: UILabel!
+    var parkingSpaces = [ParkingSpot]()
   var parkTimer: Timer?
     
     var imageArray: [UIImageView] = []
@@ -51,29 +52,19 @@ class GridUI: UIViewController {
     @IBOutlet weak var imge2: UIImageView!
     @IBOutlet weak var imge3: UIImageView!
     @IBOutlet weak var imge4: UIImageView!
-  
+  @IBOutlet weak var btnBook: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         _ = self.view
       prepareImageStacks()
+        helper()
       
   }
-//      this will change when the array reccieved
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .occupied))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .vacant))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .disabled, status: .vacant))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .vacant))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
-//      parkingSpaces.append(ParkingSpot.init(level: .ground, type: .normal, status: .occupied))
 
   override func viewDidAppear(_ animated: Bool) {
     //    this line runs the runTimedCode function continuously
     runTimedCode()
-    parkTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+    parkTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
   }
   
   @objc func runTimedCode() {
@@ -82,6 +73,9 @@ class GridUI: UIViewController {
       self.parkingSpaces = parking
         DispatchQueue.main.async {
           for (index,parkingSpot) in self.parkingSpaces.enumerated() {
+            if ( index == self.selectedIndex) {
+                continue;
+            }
           switch parkingSpot.type {
               case .disabled:
                 self.imageArray[index].image = UIImage(named: "disabled_icon.png")
@@ -91,11 +85,11 @@ class GridUI: UIViewController {
           switch parkingSpot.status {
               case .occupied: self.imageArray[index].image = UIImage(named: index < 5 ? "car_icon1.png" : "car_icon2.png")
               case .vacant: self.imageArray[index].image = nil
-          }
-        }
+                }
+            }
           //  WE WILL NEED TO DEINITIALISE THE TIMER WHEN WE MOVE TO ANOTHER AREA WITH: gameTimer?.invalidate()
+        }
     }
-}
 }
       
     @IBAction func btnBook(_ sender: Any) {
@@ -103,5 +97,28 @@ class GridUI: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "ParkingPopoverViewController") as UIViewController
         present(vc, animated: true, completion: nil)
     }
+    
+    func helper() {
+        for img in imageArray{
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            img.isUserInteractionEnabled = true
+            img.addGestureRecognizer(tapGestureRecognizer)
+        }
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        let index = imageArray.firstIndex(of: tappedImage)
+        tappedImage.image = UIImage(named: "car_selected.png")
+        selectedIndex = index ?? -1
+        if (parkingSpaces.count < selectedIndex || parkingSpaces[selectedIndex].status == ParkingSpotStatus.occupied) {
+            lblSelection.text = "That parking is not available"
+            return
+        }
+        lblSelection.text = "You selected L\(parkingSpaces[selectedIndex].levelView):P\(selectedIndex)"
+        btnBook.isEnabled = true
+        btnBook.backgroundColor = ColourTheme.Palette.primaryPurple
+    }
+    
     
 }
